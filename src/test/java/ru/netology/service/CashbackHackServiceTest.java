6d58ec2b05a43ab70996415a892ecf202eeb9af5
Aleksandra Zhukova <a.hahina@gmail.com>
2021-02-08 20:51:14 +0300

    CashbackHackService ngtest

---
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -0,0 +1,39 @@
+package ru.netology.service;
+
+import static org.testng.Assert.*;
+
+public class CashbackHackServiceTest {
+
+    @org.testng.annotations.Test
+    public void testRemain1000() {
+        CashbackHackService cashbackHackService = new CashbackHackService();
+        int amount = 1000;
+
+        int actual = cashbackHackService.remain(amount);
+        int expected = 1000;
+
+        assertEquals(actual, expected);
+    }
+
+    @org.testng.annotations.Test
+    public void testRemain1400() {
+        CashbackHackService cashbackHackService = new CashbackHackService();
+        int amount = 1400;
+
+        int actual = cashbackHackService.remain(amount);
+        int expected = 600;
+
+        assertEquals(actual, expected);
+    }
+
+    @org.testng.annotations.Test
+    public void testRemain400() {
+        CashbackHackService cashbackHackService = new CashbackHackService();
+        int amount = 400;
+
+        int actual = cashbackHackService.remain(amount);
+        int expected = 600;
+
+        assertEquals(actual, expected);
+    }
+}
